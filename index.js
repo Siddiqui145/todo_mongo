778366e39ce3fc2b82b3d4c1e94f2651ffbaa16b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-25 16:20:57 +0530

    nodemon used for auto changes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('./app');
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello USSSSSer")
+})
+
 app.listen(port, () => {
     console.log(`Server listening on Port http://localhost:${port}`);
     
