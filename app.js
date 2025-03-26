9010f6ab6d401d4a62a7ecae7cabff30bef952ba
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-26 22:55:05 +0530

    custom api & router calls for user

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const body_parser = require('body-parser'); //used to handle data that arrives from routes
+const userRouter = require('./routers/user.router')
 
 const app = express();
+
+app.use(body_parser.json());
+
+app.use('/', userRouter);
 
 module.exports = app;
