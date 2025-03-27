44d7bb04adfcbf6f730533d72ff8f7d3f8c78d66
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-27 16:46:15 +0530

    new files generation

---
+++ build/app/intermediates/cxx/Debug/6k6j4n6r/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"D:\\SetUp\\AndroidSDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\SetUp\\Flutter\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=D:\\SetUp\\AndroidSDK\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=D:\\SetUp\\AndroidSDK\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=D:\\SetUp\\AndroidSDK\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=D:\\SetUp\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\NewProjects\\todo_mongo\\build\\app\\intermediates\\cxx\\Debug\\6k6j4n6r\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\NewProjects\\todo_mongo\\build\\app\\intermediates\\cxx\\Debug\\6k6j4n6r\\obj\\armeabi-v7a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\NewProjects\\todo_mongo\\android\\app\\.cxx\\Debug\\6k6j4n6r\\armeabi-v7a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
